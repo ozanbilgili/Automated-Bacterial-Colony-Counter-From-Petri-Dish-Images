7f540afd085db03bca360c1176da434a78926aa6
ozanbilgili <ozan.bilgili@hotmail.com>
2021-09-07 20:00:55 +0300

    First commit

---
+++ ColonyCounter_Final/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  ColonyCounter_Final
+//
+//  Created by Ozan Bilgili on 3.05.2020.
+//  Copyright © 2020 Ozan Bilgili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTapButton(_ sender: Any) {
+    
+        let vc  = UIImagePickerController()
+        vc.sourceType = .photoLibrary
+        vc.delegate = self
+        vc.allowsEditing = true
+        present(vc, animated: true)
+        
+    }
+   
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+ 
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any])
+    {
+        if let image = info[UIImagePickerController.InfoKey(rawValue: "UIImagePickerControllerEditedImage")] as? UIImage{
+            imageView.image = image
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+}
